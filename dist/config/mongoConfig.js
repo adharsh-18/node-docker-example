c2dfe0bb9df67041b9375480805a6bc3e20ec310
adharsh-18 <gurudev.adharsh@gmail.com>
2024-12-18 21:02:09 +0530

    second commit

--- dist/config/mongoConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongoConfig = void 0;
-exports.mongoConfig = {
-    uri: 'mongodb://localhost:27017', // MongoDB connection string
-    dbName: 'dbexample', // Replace with your database name
-};
